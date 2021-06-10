596c988de7fee8e090c7221c64481d2b41a2deff
BlairG <blairg23@gmail.com>
2021-06-10 14:16:10 -0600

    Added support for writing missing files to the destination folder. Added data folder to gitignore. Added protected files so the script doesn't try to check contents.csv or missing.txt files

--- files_in_folder.py
+++ files_in_folder.py
@@ -10,11 +10,16 @@ exist in the right folder and a False with the filenames and hashes of the non-e
 
 import os  # Operating System functions
 import sys # System Functions
+import shutil # File copy operations
 
 import hashlib # Hashing functions
 import json # JSON stuff
 
-from time import clock # Time a function
+
+from time import localtime as clock # Time a function
+
+# Filenames we don't want to check:
+PROTECTED_FILENAMES = ['contents.csv', 'missing.txt']
 
 class FilesInFolder:
     def __init__(
@@ -26,6 +31,7 @@ class FilesInFolder:
                     hash_type='contents',
                     contents_filename='contents.json',
                     missing_files_filename='missing.txt',
+                    fix_missing_files=False,
                     verbose=False
                 ):
 
@@ -38,6 +44,7 @@ class FilesInFolder:
         self.right_folder = right_folder
         self.contents_filename = contents_filename
         self.missing_files_filename = missing_files_filename
+        self.fix_missing_files = fix_missing_files
 
         try:
             # If valid directories have not been provided:
@@ -129,13 +136,14 @@ class FilesInFolder:
             else:                               
                 filenames = self.find_filenames(directory=directory)
                 for filename in filenames:
-                    filepath = os.path.join(directory, filename)
-                    if hash_type == 'contents':
-                        hash_value = self.hash_file_contents(filepath=filepath, hash_algorithm=hash_algorithm)
-                    elif hash_type == 'filenames':
-                        hash_value = self.hash_filename(filename=filename, hash_algorithm=hash_algorithm)
-                    hashlist[str(hash_value)] = str(filepath)
-                    self.action_counter += 1
+                    if filename not in PROTECTED_FILENAMES:
+                        filepath = os.path.join(directory, filename)
+                        if hash_type == 'contents':
+                            hash_value = self.hash_file_contents(filepath=filepath, hash_algorithm=hash_algorithm)
+                        elif hash_type == 'filenames':
+                            hash_value = self.hash_filename(filename=filename, hash_algorithm=hash_algorithm)
+                        hashlist[str(hash_value)] = str(filepath)
+                        self.action_counter += 1
         except Exception as e:
             print(e)
 
@@ -170,6 +178,18 @@ class FilesInFolder:
         except Exception as e:
             print(e)
 
+    def compare_hash_lists(self, left_hash_dict=None, right_hash_dict=None):
+        '''
+        Given two hash lists, will compare them to ensure all hashes 
+        from left list exist in right list, then will return the list of
+        all hashes that are missing.
+        '''
+        missing_hash_value_filepaths = []
+        for hash_value, filepath in left_hash_dict.items():
+            if not hash_value in right_hash_dict.keys():
+                missing_hash_value_filepaths.append(filepath)
+        return missing_hash_value_filepaths
+
     def write_list_contents(self, list_contents=[], missing_files_filepath=None):
         '''
         Writes contents of a given list to a file.
@@ -185,17 +205,21 @@ class FilesInFolder:
             print(e)
 
 
-    def compare_hash_lists(self, left_hash_dict=None, right_hash_dict=None):
+    def write_missing_files(self, missing_filepaths=[], destination_directory=None):
         '''
-        Given two hash lists, will compare them to ensure all hashes 
-        from left list exist in right list, then will return the list of
-        all hashes that are missing.
+        Writes missing files to the destination filepath.
         '''
-        missing_hash_value_filepaths = []
-        for hash_value, filepath in left_hash_dict.items():
-            if not hash_value in right_hash_dict.keys():
-                missing_hash_value_filepaths.append(filepath)
-        return missing_hash_value_filepaths
+        try:
+            if missing_filepaths == []:
+                raise Exception('[ERROR] Need to provide a valid list of missing files.')
+            else:
+                for missing_filepath in missing_filepaths:
+                    missing_filename = os.path.basename(missing_filepath)
+                    # Use copy2 to retain metadata such as creation and modification times of the file
+                    destination_filepath = os.path.join(destination_directory, missing_filename)
+                    shutil.copy2(missing_filepath, destination_filepath)
+        except Exception as e:
+            print(e)
 
     def run(self):
         '''
@@ -214,11 +238,18 @@ class FilesInFolder:
                 print('Right Folder:', self.right_folder)
                 print('\n')
             else:   
-                missing_files_filepath = os.path.join(left_folder, missing_files_filename)
+                missing_files_filepath = os.path.join(self.left_folder, missing_files_filename)
                 self.write_list_contents(list_contents=missing_hash_value_filepaths, missing_files_filepath=missing_files_filepath)
                 if self.verbose:
                     print('[{action_counter}] Writing missing files to {missing_files_filepath}.\n'.format(action_counter=self.action_counter, missing_files_filepath=missing_files_filepath))
                 self.action_counter += 1
+
+                if self.fix_missing_files:
+                    self.write_missing_files(missing_filepaths=missing_hash_value_filepaths, destination_directory=self.right_folder)
+                    if self.verbose:
+                        print(f'[{self.action_counter}] Writing missing files to {self.right_folder}.\n')
+                    self.action_counter += 1
+
 
             # Left side:            
             left_outfilepath = os.path.join(self.left_folder, self.contents_filename)
@@ -250,6 +281,7 @@ if __name__ == '__main__':
     write_mode = 'csv'
     contents_filename = 'contents.csv'
     missing_files_filename = 'missing.txt'
+    fix_missing_files = True
 
     file_checker = FilesInFolder(
                                     left_folder=left_folder,
@@ -257,8 +289,9 @@ if __name__ == '__main__':
                                     write_mode=write_mode,
                                     hash_algorithm=hash_algorithm,
                                     hash_type=hash_type,
-                                    contents_filename = contents_filename,
-                                    missing_files_filename = missing_files_filename,
+                                    contents_filename=contents_filename,
+                                    missing_files_filename=missing_files_filename,
+                                    fix_missing_files=fix_missing_files,
                                     verbose=True
                                 )
 
